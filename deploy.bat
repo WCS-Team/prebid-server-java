662abfd5aeb453505deef8da36bb1c1e49aba106
Ivan <eivansavic@gmail.com>
2020-09-16 17:36:38 +0200

    Add jmx and actuator for Instana APM

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 339267708833.dkr.ecr.us-east-1.amazonaws.com
-docker build -f Dockerfile -t 339267708833.dkr.ecr.us-east-1.amazonaws.com/prebid:v1.0.1 .
-docker push 339267708833.dkr.ecr.us-east-1.amazonaws.com/prebid:v1.0.1
+docker build -f Dockerfile -t 339267708833.dkr.ecr.us-east-1.amazonaws.com/prebid:v1.1.0 .
+docker push 339267708833.dkr.ecr.us-east-1.amazonaws.com/prebid:v1.1.0
